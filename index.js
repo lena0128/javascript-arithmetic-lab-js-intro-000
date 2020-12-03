e51e6d96c3d66883334bee772f0099ff861dcca7
Lena Hou <lenahou2013@gmail.com>
2020-12-03 19:16:10 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function add() {
+  return 1 + 81;
+}
+
+function subtract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
+
+function increment(){
+
+}
+
+function decrement(){
+
+}
