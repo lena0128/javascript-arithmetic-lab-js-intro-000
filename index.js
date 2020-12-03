c8dea02f774853a75afd07e33cf71e68e6a0f64b
Lena Hou <lenahou2013@gmail.com>
2020-12-03 19:26:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,3 +21,11 @@ return n++;
 function decrement(n){
 return n--;
 }
+
+function makeInt(string){
+  return parseInt(string, 10);
+}
+
+function preserveDecimal(string) {
+  return parseFloat(string);
+}
